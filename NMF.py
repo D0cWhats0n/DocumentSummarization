a75f4b1ddc3f34c10e7ef0c5b9551a856c22cc26
Johannes Märkle <johannes.markle@axoom.com>
2017-04-04 16:39:39 +0200

    removing numbers; Introducing alpha and betha in LDA

--- NMF.py
+++ NMF.py
@@ -4,15 +4,22 @@ Created on Tue Mar 07 15:03:48 2017
 
 @author: JohannesM
 """
-
+from pandas.tools.plotting import scatter_matrix
 import numpy as np  # a conventional alias
 import pandas as pd
 import sklearn.feature_extraction.text as text
 from sklearn import decomposition
+from string import digits
 
-num_topics = 5
-num_top_words = 10
-num_features = 1000
+num_topics = 11
+num_top_words = 15
+
+#get stop words
+stop_file = open("stop_words.txt","r")
+stop_words = stop_file.read().decode('latin-1').split('\n')
+
+print stop_words
+
 
 def print_top_words(clf, vectorizer):
     topic_words = []
@@ -27,25 +34,32 @@ def print_top_words(clf, vectorizer):
 
 def NMF(documents, vectorizer):
     dtm = vectorizer.fit_transform(documents).toarray()
-    vectorizer.fit_transform(documents)
     
     clf = decomposition.NMF(n_components=num_topics, random_state=1)
-    clf.fit_transform(dtm)
+    matrix=clf.fit_transform(dtm)
     
     print_top_words(clf, vectorizer)
+    
+    return matrix
 
     
 def Dirichlet(documents, vectorizer):
     dtm = vectorizer.fit_transform(documents).toarray()
-    vectorizer.fit_transform(documents)
-    
-    lda = decomposition.LatentDirichletAllocation(n_topics=num_topics, max_iter=5,
+
+
+    lda = decomposition.LatentDirichletAllocation(n_topics=num_topics, max_iter=50,
                                 learning_method='online',
                                 learning_offset=50.,
-                                random_state=0)
+                                random_state=0,
+                                verbose=1,
+                                evaluate_every=1,                                
+                                doc_topic_prior=0.2,
+                                topic_word_prior = 0.6)
     lda.fit(dtm)
-    
+
     print_top_words(lda,vectorizer)
+    
+    return lda
 
 def concatString(stringArr):
     concatString = ""
@@ -63,26 +77,40 @@ dates = p08_df.drop_duplicates('Datum')['Datum']
 #Concatinate strings of same document and store them in documents
 documents = []    
 #vectorizer = text.CountVectorizer(max_df=0.2 ,min_df=0)   
-Tfidf = text.TfidfVectorizer(max_df=0.2, min_df=0)              
-CountVector =  text.CountVectorizer(max_df=0.2 ,min_df=0)  
+Tfidf = text.TfidfVectorizer(max_df=0.9, min_df=0.2, stop_words = stop_words)              
+CountVector =  text.CountVectorizer(max_df=0.9 ,min_df=0.2,  stop_words = stop_words)  
+
 
 for date in dates:
     total_text = ""
     for cellText in p08_df[p08_df['Datum']==date]['Text'].values:
         total_text += str(cellText) + '\n'
+    #remove all numbers 
+    total_text = total_text.translate(None, digits)    
     documents.append(total_text)
 
 
+#dropping english topics
+documents.pop(9)
+documents.pop(8)
+
+ 
+
 print("\nCount weights Non-Negative Matrix factorization")
-NMF(documents, CountVector)
+NMF_clf = NMF(documents, CountVector)
 print("\nTFIDF word weights Non-Negative Matrix factorization")
-NMF(documents, Tfidf)
+NMF_tfidf_clf = NMF(documents, Tfidf)
 print("\nCount weights Latent Dirichlet Allocation")
 Dirichlet(documents,CountVector)
-print("\nTFIDF Latent Dirichlet Allocation")
-Dirichlet(documents,Tfidf)
+
+
+#plot scatter plots for 2 topics with NMF
+#df_columns=['topic1','topic2','topic3','topic4','topic5']
 
 
 
+#NMF_df=pd.DataFrame(data=NMF_tfidf_clf,    # values
+              #columns=df_columns)
 
+#scatter_matrix(NMF_df,alpha=0.8, figsize=(6, 6))
 #len(vocab)
